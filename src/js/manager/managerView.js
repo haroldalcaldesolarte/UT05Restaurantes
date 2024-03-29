097b61483b811b7a91382fe76fe58a7c550326a6
halcalde <haroldalcaldesolarte@gmail.com>
2024-03-29 21:02:35 +0100

    modificacion css
    modificamos el html y css y se añade en el menu lateral las categorias cuando se carga la pagina

--- src/js/manager/managerView.js
+++ src/js/manager/managerView.js
@@ -1,22 +1,26 @@
 class ManagerView {
   constructor(){
     this.main = document.getElementById('principal');
+    this.aside = document.getElementById('menu_lateral');
   }
 
   init(categories) {//en este init debo recoger las categorias y listarlas antes se debe hacer en OnLoad
     this.main.replaceChildren();
+    this.aside.replaceChildren();
+    let asideContent = '';
     for (const category of categories){
-      console.log(category.getName());
-      this.main.insertAdjacentHTML('afterbegin',`<div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">${category.getName()}</h5>
-        <p class="card-text">${category.getDescription()}</p>
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
-      </div>
-    </div>`
-    );
+      this.main.insertAdjacentHTML('afterbegin',
+      `<div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${category.getName()}</h5>
+          <p class="card-text">${category.getDescription()}</p>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>`);
+      asideContent += `<li><a href="#">${category.getName()}</a></li>`;
     }
+    this.aside.innerHTML = `<ul>${asideContent}</ul>`;
   }
 
   bindInit(handler){
